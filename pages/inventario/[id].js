11c8bb551c9535332e85109c8465f73be971e3d9
Victor hernandez <vhernandez.99@hotmail.com>
2022-04-25 14:34:19 -0500

    new endpoint

--- pages/inventario/[id].js
+++ pages/inventario/[id].js
@@ -6,7 +6,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import Swal from "sweetalert2";
 import moment from "moment";
-export default function edit() {
+export default function Edit() {
   const router = useRouter();
   const id = Number(router.query.id);
   const { jwt } = useGetUserInfo();
